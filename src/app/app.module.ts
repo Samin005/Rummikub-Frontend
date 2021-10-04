0c7df8e540208415895268020df6bd670bca1d97
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-10-04 18:08:58 +0600

    Added tsparticles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TsParticlesComponent } from './ts-particles/ts-particles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TsParticlesComponent
   ],
   imports: [
     BrowserModule,
